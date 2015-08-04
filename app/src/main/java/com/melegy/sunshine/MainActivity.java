fef4bb1a79aada379c7bc9f429b79c49c4e088cc
Ahmad <ahmad.melegy@gmail.com>
2015-08-05 00:41:03 +0200

    Make MainActivity extends ActionBarActivity

--- app/src/main/java/com/melegy/sunshine/MainActivity.java
+++ app/src/main/java/com/melegy/sunshine/MainActivity.java
@@ -1,11 +1,11 @@
 package com.melegy.sunshine;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
