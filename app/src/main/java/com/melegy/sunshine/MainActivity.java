40263957ba4dd8e2a749fd08eb2f7843e2dd04b9
Ahmad <ahmad.melegy@gmail.com>
2015-08-04 15:08:49 +0200

    Add dummy data

--- app/src/main/java/com/melegy/sunshine/MainActivity.java
+++ app/src/main/java/com/melegy/sunshine/MainActivity.java
@@ -1,11 +1,11 @@
 package com.melegy.sunshine;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
