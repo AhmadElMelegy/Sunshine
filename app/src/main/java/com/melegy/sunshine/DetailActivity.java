9a54478c3298e7d1a37422eb33fda9426ccf7c09
Ahmad <ahmad.melegy@gmail.com>
2015-08-08 17:01:16 +0200

    Launch SettingsActivity from menus

--- app/src/main/java/com/melegy/sunshine/DetailActivity.java
+++ app/src/main/java/com/melegy/sunshine/DetailActivity.java
@@ -41,7 +41,8 @@ public class DetailActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
         }
 
         return super.onOptionsItemSelected(item);
