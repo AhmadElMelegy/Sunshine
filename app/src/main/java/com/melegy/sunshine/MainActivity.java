f27d4464b72c4c3c5ebcef3b0e4837380836edc2
Ahmad <ahmad.melegy@gmail.com>
2015-09-01 23:44:43 +0200

    Clean code & Update SunshineSyncAdapter

--- app/src/main/java/com/melegy/sunshine/MainActivity.java
+++ app/src/main/java/com/melegy/sunshine/MainActivity.java
@@ -9,6 +9,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.melegy.sunshine.sync.SunshineSyncAdapter;
+
 public class MainActivity extends ActionBarActivity implements ForecastFragment.Callback {
     private static final String DETAILFRAGMENT_TAG = "DFTAG";
     String mLocation;
@@ -35,6 +37,8 @@ public class MainActivity extends ActionBarActivity implements ForecastFragment.
         ForecastFragment forecastFragment = (ForecastFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.fragment_forecast);
         forecastFragment.setUseTodayLayout(!mTwoPane);
+
+        SunshineSyncAdapter.initializeSyncAdapter(this);
     }
 
     @Override
